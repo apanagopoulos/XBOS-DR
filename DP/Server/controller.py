280636fa866d6579033edbd53b0679b5183e70c9
Daniel Lengyel <daniel.lengyel@berkeley.edu>
2018-07-10 18:34:36 +0200

    Need to have south=berkeley-senior-center as a special case. can't train a thermal model for it

--- DP/Server/controller.py
+++ DP/Server/controller.py
@@ -325,21 +325,22 @@ if __name__ == '__main__':
     # only single stage cooling buildings get to retrive data. otherwise takes too long.
     # if building in ["north-berkeley-senior-center", "ciee", "avenal-veterans-hall", "orinda-community-center",
     #                 "avenal-recreation-center", "word-of-faith-cc", "jesse-turner-center", "berkeley-corporate-yard"]:
-    thermal_data = utils.get_data(cfg=cfg, client=client, days_back=150, force_reload=False)
+    if building != "south-berkeley-senior-center":
+        thermal_data = utils.get_data(cfg=cfg, client=client, days_back=150, force_reload=False)
 
-    zone_thermal_models = {}
-    for zone, zone_data in thermal_data.items():
-        # Concat zone data to put all data together and filter such that all datapoints have dt != 1
-        filtered_zone_data = zone_data[zone_data["dt"] == 5]
-        # print(zone)
-        # print(filtered_zone_data.shape)
-        if zone != "HVAC_Zone_Please_Delete_Me":
-            zone_thermal_models[zone] = MPCThermalModel(zone=zone, thermal_data=filtered_zone_data,
-                                                    interval_length=15, thermal_precision=0.05)
-    # else:
-    #     zone_thermal_models = {}
-    #     for zone in tstats.keys():
-    #         zone_thermal_models[zone] = None
+        zone_thermal_models = {}
+        for zone, zone_data in thermal_data.items():
+            # Concat zone data to put all data together and filter such that all datapoints have dt != 1
+            filtered_zone_data = zone_data[zone_data["dt"] == 5]
+            # print(zone)
+            # print(filtered_zone_data.shape)
+            if zone != "HVAC_Zone_Please_Delete_Me":
+                zone_thermal_models[zone] = MPCThermalModel(zone=zone, thermal_data=filtered_zone_data,
+                                                        interval_length=15, thermal_precision=0.05)
+    else:
+        zone_thermal_models = {}
+        for zone in tstats.keys():
+            zone_thermal_models[zone] = None
 
 
     print("Trained Thermal Model")
