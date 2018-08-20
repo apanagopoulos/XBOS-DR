a12b1622f4a41182308df79b3c01e63ff0dcfbd2
Gabe Fierro <gtfierro225@gmail.com>
2018-08-20 16:14:03 -0700

    Add Pipfile + setup.py
    Also adjusted imports for MPC package. Hopefully this should make this
    easier to deploy

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='drcontroller',
+      version='0.0.1',
+      description='DR controller',
+      url='https://github.com/gtfierro/XBOS-DR',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
+
+
